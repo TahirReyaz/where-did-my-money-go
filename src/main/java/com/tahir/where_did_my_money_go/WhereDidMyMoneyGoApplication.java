58a11683e0c2f9f3d12482942fb7bab39de6d68a
TahirReyaz <tahirreyaz5@gmail.com>
2026-03-27 16:28:19 +0530

    Initial spring starter code

---
+++ src/main/java/com/tahir/where_did_my_money_go/WhereDidMyMoneyGoApplication.java
@@ -0,0 +1,13 @@
+package com.tahir.where_did_my_money_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereDidMyMoneyGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereDidMyMoneyGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahir/where_did_my_money_go/WhereDidMyMoneyGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahir.where_did_my_money_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereDidMyMoneyGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
